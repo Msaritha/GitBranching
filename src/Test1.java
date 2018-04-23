cd92251f53808e578d2229cb98dda0a60ae47902
Msaritha <saritha.pavithram@gmail.com>
2018-04-23 16:52:23 -0700

    Commiting into Branch1 remotely

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,7 @@
 
 public class Test1 {
 
-	public static void main(String args[])
-	{
-		System.out.println("First file to coomit");
+	public static void main(String[] args) {
+		System.out.println("Test file");
 	}
 }
